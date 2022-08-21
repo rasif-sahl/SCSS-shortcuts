08d2e114075c595632dca0d36b005070ac10140d
Rasif The Legend <rasif.outdesk@gmail.com>
2022-08-21 20:38:09 +0530

    project set-up

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # SCSS Shortcuts
+
+This is an simple project that will help developers to find short-cuts when developing complex systems using SCSS. 
+
+And to demonstrate how each section is worked we have used the react framework. 
+
+But you can use the required piece of code in any project you are using. (But first according to the project you will have to install the SCSS dependencies) 
+
+## How to set-up this project in your local mashing
+
+* First install npm globally
+* and then `Fork` the repo. 
+* and then `clone` the repo.
+* run `npm install`
+* run `npm start`
+
+## Project Structure
--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BreakPoints from "./BreakPoints/Example/Index.jsx";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<BreakPoints />} />
+        </Routes>
+     </Router>
+      </div>
     </div>
   );
 }
